36caafdef19df4aa6af748ecfbbcb8771ed76018
Ashton Gardiner <acrgardiner@gmail.com>
2025-08-28 16:38:12 +1200

    Username edit

--- Identity/IUserService.cs
+++ Identity/IUserService.cs
@@ -5,6 +5,7 @@
         Task<List<ApplicationUser>> GetUsersAsync();
         Task<int> GetUserCountAsync();
         Task<ApplicationUser> AddUser(string username, string password, List<Roles> roles);
+        Task<bool> UpdateUserName(string userId, string newUsername);
         Task<bool> ResetPasswordAsync(string userId, string newPassword);
     }
 }
