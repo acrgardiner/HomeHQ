1a75f0d0ab686c2f426b9c28079eff602aa6c4d5
Ashton Gardiner <acrgardiner@gmail.com>
2025-09-10 16:00:34 +1200

    File re-upload logic changes

--- FileStorage/IFileStorageService.cs
+++ FileStorage/IFileStorageService.cs
@@ -6,8 +6,7 @@ namespace projectaardvarkx2.FileStorage
     public interface IFileStorageService
     {
         Task<string> UploadAsync<T>(IBrowserFile? file) where T : class;
-        Task<(string, float)> UploadAsync<T>(byte[] fileBytes, string sourceFilename, string contentType) where T : class;
-        Task<(string, float)> ReUploadAsync<T>(Attachment attachment, byte[] fileBytes) where T : class;
+        Task<bool> UploadAsync<T>(byte[] fileBytes, Attachment attachment) where T : class;
         Task<string> UploadAsync<T>(string localFile, string contentType) where T : class;
     }
 }
