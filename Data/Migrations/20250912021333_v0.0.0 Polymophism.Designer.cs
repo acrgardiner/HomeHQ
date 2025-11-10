50b3cea7a70307a4cb756a94ea3a1eeb3de30fc0
Ashton Gardiner <acrgardiner@gmail.com>
2025-11-11 09:48:05 +1300

    Naming cleanup

--- Data/Migrations/20250912021333_v0.0.0 Polymophism.Designer.cs
+++ Data/Migrations/20250912021333_v0.0.0 Polymophism.Designer.cs
@@ -122,7 +122,7 @@ namespace HomeHQ.data.migrations
                     b.ToTable("UserTokens", (string)null);
                 });
 
-            modelBuilder.Entity("projectaardvarkx2.Entities.Asset", b =>
+            modelBuilder.Entity("homehq.Entities.Asset", b =>
                 {
                     b.Property<Guid>("Id")
                         .ValueGeneratedOnAdd()
@@ -176,7 +176,7 @@ namespace HomeHQ.data.migrations
                     b.ToTable("Assets");
                 });
 
-            modelBuilder.Entity("projectaardvarkx2.Entities.Attachment", b =>
+            modelBuilder.Entity("homehq.Entities.Attachment", b =>
                 {
                     b.Property<Guid>("Id")
                         .ValueGeneratedOnAdd()
@@ -241,7 +241,7 @@ namespace HomeHQ.data.migrations
                     b.ToTable("Attachments");
                 });
 
-            modelBuilder.Entity("projectaardvarkx2.Entities.AttachmentType", b =>
+            modelBuilder.Entity("homehq.Entities.AttachmentType", b =>
                 {
                     b.Property<Guid>("Id")
                         .ValueGeneratedOnAdd()
@@ -279,7 +279,7 @@ namespace HomeHQ.data.migrations
                     b.ToTable("AttachmentTypes");
                 });
 
-            modelBuilder.Entity("projectaardvarkx2.Entities.AttributeValue", b =>
+            modelBuilder.Entity("homehq.Entities.AttributeValue", b =>
                 {
                     b.Property<Guid>("Id")
                         .ValueGeneratedOnAdd()
@@ -326,7 +326,7 @@ namespace HomeHQ.data.migrations
                     b.ToTable("Attributes");
                 });
 
-            modelBuilder.Entity("projectaardvarkx2.Entities.Category", b =>
+            modelBuilder.Entity("homehq.Entities.Category", b =>
                 {
                     b.Property<Guid>("Id")
                         .ValueGeneratedOnAdd()
@@ -364,7 +364,7 @@ namespace HomeHQ.data.migrations
                     b.ToTable("Categories");
                 });
 
-            modelBuilder.Entity("projectaardvarkx2.Entities.Note", b =>
+            modelBuilder.Entity("homehq.Entities.Note", b =>
                 {
                     b.Property<Guid>("Id")
                         .ValueGeneratedOnAdd()
@@ -413,7 +413,7 @@ namespace HomeHQ.data.migrations
                     b.ToTable("Notes");
                 });
 
-            modelBuilder.Entity("projectaardvarkx2.Entities.WarrantyType", b =>
+            modelBuilder.Entity("homehq.Entities.WarrantyType", b =>
                 {
                     b.Property<Guid>("Id")
                         .ValueGeneratedOnAdd()
@@ -460,7 +460,7 @@ namespace HomeHQ.data.migrations
                     b.ToTable("WarrantyTypes");
                 });
 
-            modelBuilder.Entity("projectaardvarkx2.Identity.ApplicationRole", b =>
+            modelBuilder.Entity("homehq.Identity.ApplicationRole", b =>
                 {
                     b.Property<string>("Id")
                         .HasColumnType("TEXT");
@@ -486,7 +486,7 @@ namespace HomeHQ.data.migrations
                     b.ToTable("Roles", (string)null);
                 });
 
-            modelBuilder.Entity("projectaardvarkx2.Identity.ApplicationUser", b =>
+            modelBuilder.Entity("homehq.Identity.ApplicationUser", b =>
                 {
                     b.Property<string>("Id")
                         .HasColumnType("TEXT");
@@ -543,7 +543,7 @@ namespace HomeHQ.data.migrations
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
                 {
-                    b.HasOne("projectaardvarkx2.Identity.ApplicationRole", null)
+                    b.HasOne("homehq.Identity.ApplicationRole", null)
                         .WithMany()
                         .HasForeignKey("RoleId")
                         .OnDelete(DeleteBehavior.SetNull)
@@ -552,7 +552,7 @@ namespace HomeHQ.data.migrations
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserClaim<string>", b =>
                 {
-                    b.HasOne("projectaardvarkx2.Identity.ApplicationUser", null)
+                    b.HasOne("homehq.Identity.ApplicationUser", null)
                         .WithMany()
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.SetNull)
@@ -561,7 +561,7 @@ namespace HomeHQ.data.migrations
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserLogin<string>", b =>
                 {
-                    b.HasOne("projectaardvarkx2.Identity.ApplicationUser", null)
+                    b.HasOne("homehq.Identity.ApplicationUser", null)
                         .WithMany()
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.SetNull)
@@ -570,13 +570,13 @@ namespace HomeHQ.data.migrations
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserRole<string>", b =>
                 {
-                    b.HasOne("projectaardvarkx2.Identity.ApplicationRole", null)
+                    b.HasOne("homehq.Identity.ApplicationRole", null)
                         .WithMany()
                         .HasForeignKey("RoleId")
                         .OnDelete(DeleteBehavior.SetNull)
                         .IsRequired();
 
-                    b.HasOne("projectaardvarkx2.Identity.ApplicationUser", null)
+                    b.HasOne("homehq.Identity.ApplicationUser", null)
                         .WithMany()
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.SetNull)
@@ -585,21 +585,21 @@ namespace HomeHQ.data.migrations
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserToken<string>", b =>
                 {
-                    b.HasOne("projectaardvarkx2.Identity.ApplicationUser", null)
+                    b.HasOne("homehq.Identity.ApplicationUser", null)
                         .WithMany()
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.SetNull)
                         .IsRequired();
                 });
 
-            modelBuilder.Entity("projectaardvarkx2.Entities.Asset", b =>
+            modelBuilder.Entity("homehq.Entities.Asset", b =>
                 {
-                    b.HasOne("projectaardvarkx2.Entities.Category", "Category")
+                    b.HasOne("homehq.Entities.Category", "Category")
                         .WithMany()
                         .HasForeignKey("CategoryId")
                         .OnDelete(DeleteBehavior.SetNull);
 
-                    b.HasOne("projectaardvarkx2.Entities.WarrantyType", "WarrantyType")
+                    b.HasOne("homehq.Entities.WarrantyType", "WarrantyType")
                         .WithMany()
                         .HasForeignKey("WarrantyTypeId")
                         .OnDelete(DeleteBehavior.SetNull);
@@ -609,9 +609,9 @@ namespace HomeHQ.data.migrations
                     b.Navigation("WarrantyType");
                 });
 
-            modelBuilder.Entity("projectaardvarkx2.Entities.Attachment", b =>
+            modelBuilder.Entity("homehq.Entities.Attachment", b =>
                 {
-                    b.HasOne("projectaardvarkx2.Entities.AttachmentType", "AttachmentType")
+                    b.HasOne("homehq.Entities.AttachmentType", "AttachmentType")
                         .WithMany()
                         .HasForeignKey("AttachmentTypeId")
                         .OnDelete(DeleteBehavior.SetNull);
--- Dockerfile
+++ Dockerfile
@@ -17,16 +17,16 @@ EXPOSE 8081
 FROM mcr.microsoft.com/dotnet/sdk:9.0-alpine AS build
 ARG BUILD_CONFIGURATION=Release
 WORKDIR /src
-COPY ["projectaardvarkx2.csproj", "."]
-RUN dotnet restore "./projectaardvarkx2.csproj"
+COPY ["homehq.csproj", "."]
+RUN dotnet restore "./homehq.csproj"
 COPY . .
 WORKDIR "/src/."
-RUN dotnet build "./projectaardvarkx2.csproj" -c $BUILD_CONFIGURATION -o /app/build
+RUN dotnet build "./homehq.csproj" -c $BUILD_CONFIGURATION -o /app/build
 
 # This stage is used to publish the service project to be copied to the final stage
 FROM build AS publish
 ARG BUILD_CONFIGURATION=Release
-RUN dotnet publish "./projectaardvarkx2.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+RUN dotnet publish "./homehq.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
 
 # This stage is used in production or when running from VS in regular mode (Default when not using the Debug configuration)
 FROM base AS final
@@ -35,4 +35,4 @@ ENV COMMIT_SHA=$COMMIT_SHA
 
 WORKDIR /app
 COPY --from=publish /app/publish .
-ENTRYPOINT ["dotnet", "projectaardvarkx2.dll"]
+ENTRYPOINT ["dotnet", "homehq.dll"]
