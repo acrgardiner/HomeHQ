33f6d760f1cd55ad7086935a1563b63a790ba8e6
Ashton Gardiner <acrgardiner@gmail.com>
2025-11-26 09:29:17 +1300

    Alternate for attempt for camera timeouts

--- Program.cs
+++ Program.cs
@@ -55,18 +55,19 @@ try
         .AddHubOptions(options =>
         {
             options.MaximumReceiveMessageSize = 10 * 1024 * 1024;
-            // Keep connection alive during long operations like camera capture
-            options.ClientTimeoutInterval = TimeSpan.FromSeconds(60);
-            options.KeepAliveInterval = TimeSpan.FromSeconds(15);
+            // Extended timeouts for camera operations - give users plenty of time
+            options.ClientTimeoutInterval = TimeSpan.FromMinutes(2);
+            options.HandshakeTimeout = TimeSpan.FromSeconds(30);
+            options.KeepAliveInterval = TimeSpan.FromSeconds(10);
         });
 
     // Configure circuit options for better handling of disconnections and long-running JS operations
-    //builder.Services.Configure<Microsoft.AspNetCore.Components.Server.CircuitOptions>(options =>
-    //{
-    //    options.DisconnectedCircuitRetentionPeriod = TimeSpan.FromMinutes(3);
-    //    options.DisconnectedCircuitMaxRetained = 100;
-    //    options.JSInteropDefaultCallTimeout = TimeSpan.FromMinutes(1);
-    //});
+    builder.Services.Configure<Microsoft.AspNetCore.Components.Server.CircuitOptions>(options =>
+    {
+        options.DisconnectedCircuitRetentionPeriod = TimeSpan.FromMinutes(5);
+        options.DisconnectedCircuitMaxRetained = 100;
+        options.JSInteropDefaultCallTimeout = TimeSpan.FromMinutes(2);
+    });
 
     builder.Services.AddCascadingAuthenticationState();
     builder.Services.AddScoped<IdentityUserAccessor>();
